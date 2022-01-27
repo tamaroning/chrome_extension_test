65d3977be98110c73ece5cf8ee7c745bc2e4fbee
tamaron <tamaron1203@gmail.com>
2022-01-27 14:27:06 +0900

    add popup and button

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-function showAlert() {
-	alert("Hello");
-}
-chrome.action.onClicked.addListener((tab) => {
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: showAlert
+function main() {
+	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+		let url = tabs[0].url;
+		// use `url` here inside the callback because it's asynchronous!
+		alert(url);
 	});
-});
+	//alert("Hello");
+}
+
+document.getElementById("alertButton").onclick = function() {
+	showAlert();
+};
